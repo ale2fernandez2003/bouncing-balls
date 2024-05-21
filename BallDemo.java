79f0c9b00f8bf697fcc31498fd8a8426843a31ad
Alejandro <ale2fernandez2003@gmail.com>
2024-05-21 11:56:32 +0200

    inicial

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,5 @@
 import java.awt.Color;
-
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -23,30 +23,39 @@ public class BallDemo
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int numBalls)
     {
         int ground = 400;   // position of the ground line
-
+        
+        Random random = new Random();
+        
+        
         myCanvas.setVisible(true);
 
         // draw the ground
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
-
+        BouncingBall[] balls = new BouncingBall[numBalls];
+        for (int i = 0; i < numBalls; i++) {
+            int radio = random.nextInt(20) + 10;
+            Color color = new Color(random.nextInt(256), random.nextInt(256), random.nextInt(256));
+            int X = random.nextInt(201) + 200;
+            balls[i] = new BouncingBall(X, 50, radio, color, ground, myCanvas);
+            balls[i].draw();
+        }
+        
         // make them bounce
         boolean finished =  false;
         while(!finished) {
             myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
-            // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for (BouncingBall ball : balls) {
+                ball.move();
+                if (ball.getXPosition() >= 550) {
+                    finished = true;
+                }
+                // stop once ball has travelled a certain distance on x axis
+
             }
         }
     }
